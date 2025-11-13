1a3dc0d9379a8f6aac4cd390962c37e698dfd49d
wldnxd <wildanaish0614@gmail.com>
2025-11-13 13:50:22 +0700

    Update db.php

--- lamanadmin/db.php
+++ lamanadmin/db.php
@@ -3,7 +3,7 @@
 define('DB_HOST', '127.0.0.1');
 define('DB_USER', 'root'); // Ganti jika user Anda bukan root
 define('DB_PASS', '');     // Ganti jika Anda punya password
-define('DB_NAME', 'wildan');
+define('DB_NAME', 'umkm_barber');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
